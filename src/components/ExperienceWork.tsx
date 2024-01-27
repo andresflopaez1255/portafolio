930a824654b5220d841b61c2da9b5609d35cb3e7
aflorezp <aflorezp@intergrupo.com>
2024-01-27 15:05:16 -0500

    fixed

--- src/components/ExperienceWork.tsx
+++ src/components/ExperienceWork.tsx
@@ -1,5 +1,4 @@
 import  { useState } from "react";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import useWorkExperience from "../hooks/work-experience/useWorkInfo";
--- src/components/Tecnologies.tsx
+++ src/components/Tecnologies.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import useTecnologies from "../hooks/tecnologies/useTecnologies";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
