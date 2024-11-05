59ed888abffca081cfe7e7f87071ec2d30af9919
Andres florez <andresflopaez@gmail.com>
2024-11-05 11:23:14 -0500

    fixed

--- src/components/PlayerVideo.tsx
+++ src/components/PlayerVideo.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 
 const VideoPlayerCustomButton = () => {
   const [playing, setPlaying] = useState(false);
@@ -8,8 +8,10 @@ const VideoPlayerCustomButton = () => {
   const togglePlay = () => {
     if (videoRef.current) {
       if (playing) {
+        //@ts-ignore
         videoRef.current.pause();
       } else {
+        //@ts-ignore
         videoRef.current.play();
       }
       setPlaying(!playing);
@@ -25,32 +27,53 @@ const VideoPlayerCustomButton = () => {
     };
 
     const handlePlay = () => {
-        setPlaying(true); // Vuelve a mostrar el botón cuando el video está en pausa
-      };
+      setPlaying(true); // Vuelve a mostrar el botón cuando el video está en pausa
+    };
 
     const handleEnded = () => {
       setPlaying(false); // Vuelve a mostrar el botón cuando el video termina
     };
 
     if (video) {
-        video.addEventListener('play', handlePlay);
-      video.addEventListener('pause', handlePause);
-      video.addEventListener('ended', handleEnded);
+      //@ts-ignore
+
+      video.addEventListener("play", handlePlay);
+      //@ts-ignore
+
+      video.addEventListener("pause", handlePause);
+      //@ts-ignore
+
+      video.addEventListener("ended", handleEnded);
     }
 
     return () => {
       if (video) {
-        video.removeEventListener('play', handlePlay);
-        video.removeEventListener('pause', handlePause);
-        video.removeEventListener('ended', handleEnded);
+        //@ts-ignore
+
+        video.removeEventListener("play", handlePlay);
+        //@ts-ignore
+
+        video.removeEventListener("pause", handlePause);
+        //@ts-ignore
+
+        video.removeEventListener("ended", handleEnded);
       }
     };
   }, []);
 
   return (
     <div className="video-container">
-      <video ref={videoRef} controls={playing ? true : false} className="video" height={350} width="100%">
-        <source src="https://firebasestorage.googleapis.com/v0/b/portafolio-dd96b.appspot.com/o/images%2Fexample%20yms.mov?alt=media&token=769bb42c-ce40-4b3e-a7b7-c06e2fef69f4" type="video/mp4" />
+      <video
+        ref={videoRef}
+        controls={playing ? true : false}
+        className="video"
+        height={350}
+        width="100%"
+      >
+        <source
+          src="https://firebasestorage.googleapis.com/v0/b/portafolio-dd96b.appspot.com/o/images%2Fexample%20yms.mov?alt=media&token=769bb42c-ce40-4b3e-a7b7-c06e2fef69f4"
+          type="video/mp4"
+        />
         Tu navegador no soporta el formato de video.
       </video>
       {!playing && (
